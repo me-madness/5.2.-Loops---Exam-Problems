3f02d97a7abfa92e228b980ab8c9b250b96a16a0
me_madness <p_rangel@abv.bg>
2023-08-27 16:48:12 +0200

    01.Histogram finish

--- 01. Histogram.py
+++ 01. Histogram.py
@@ -13,24 +13,25 @@ p5_count = 0
 
 for i in range(n):
     current_num = int(input())
+    
     if current_num < 200:
         p1_count += 1
-    elif current_num >= 200 and current_num < 399:
+    elif current_num >= 200 and current_num <= 399:
         p2_count += 1
-    elif current_num >= 400 and current_num < 599:
+    elif current_num >= 400 and current_num <=599:
         p3_count += 1
-    elif current_num >= 600 and current_num < 799:
+    elif current_num >= 600 and current_num <= 799:
         p4_count += 1
     else:
         p5_count +=1
-p1 = (p1_count / n) * 100        
-p1 = (p2_count / n) * 100        
-p1 = (p3_count / n) * 100        
-p1 = (p4_count / n) * 100        
-p1 = (p5_count / n) * 100        
+p1 = p1_count * 100 / n       
+p2 = p2_count * 100 / n        
+p3 = p3_count * 100 / n        
+p4 = p4_count * 100 / n        
+p5 = p5_count * 100 / n       
             
-print(f"{round(p1)} % ")
-print(f"{round(p2)} % ")
-print(f"{round(p3)} % ")
-print(f"{round(p4)} % ")
-print(f"{round(p5)} % ")
+print("%.2f" % p1 + "%")
+print("%.2f" % p2 + "%")
+print("%.2f" % p3 + "%")
+print("%.2f" % p4 + "%")
+print("%.2f" % p5 + "%")
