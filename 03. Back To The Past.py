8c5458f95fd18e3388ab655b0cf6d78b8ed99fd4
me-madness <58656549+me-madness@users.noreply.github.com>
2023-08-28 22:30:50 +0200

    03.2

--- 03. Back To The Past.py
+++ 03. Back To The Past.py
@@ -2,3 +2,4 @@ money_past = float(input())
 year_in_past = int(input())
 
 print("Yes! He will live a carefree life and will have {rest_money} dollars left.")
+print("He will need {need_money_to_survive} dollars to survive.")
