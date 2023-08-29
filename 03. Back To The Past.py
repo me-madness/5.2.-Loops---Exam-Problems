0316f064247dea4e4c014f40b3c356cae1218846
me_madness <p_rangel@abv.bg>
2023-08-29 19:01:10 +0200

    03.Back to the Past

--- 03. Back To The Past.py
+++ 03. Back To The Past.py
@@ -1,5 +1,15 @@
 money_past = float(input())
 year_in_past = int(input())
+years = 18
 
-print("Yes! He will live a carefree life and will have {rest_money} dollars left.")
-print("He will need {need_money_to_survive} dollars to survive.")
+for current_year in range(1800, year_in_past+1):
+    if current_year % 2 == 0:
+        money_past -= 12000
+    else:
+        money_past -= (12000 + 50 * years)
+    years += 1    
+        
+if money_past > 0:
+    print(f"Yes! He will live a carefree life and will have {money_past} dollars left.")
+else:            
+    print(f"He will need {abs(money_past)} dollars to survive.")
